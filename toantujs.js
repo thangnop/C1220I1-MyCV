a2841be1abcbd5ebf977fd87fa70b05efd624b99
thangnop <you@example.com>
2020-12-23 09:35:45 +0700

    fixup! hot

---
+++ toantujs.js
@@ -0,0 +1,9 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the Width");
+inputHeight = prompt("Enter the Height");
+let width = parseInt(inputWidth);
+let heigh = parseInt(inputHeight);
+let area = width*heigh;
+
+document.write("The area is: " +area);
